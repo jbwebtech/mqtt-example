514c0ecf97764d5fcfe54058efa9a745ccfdc255
jbwebtech <justinwbianchi@gmail.com>
2025-03-21 16:32:49 -0400

    Update app service and HTML UI

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 7979;
-const mqttPort = process.env.MQTT_PORT || 1883;
 const mockData = require('./mockData');
 const mqtt = require('mqtt');
 
-// MQTT client setup
-const mqttClient = mqtt.connect(`mqtt://localhost:${mqttPort}`);
+// Environment variables
+const port = process.env.PORT || 7979;
+const mqttHost = process.env.MQTT_HOST || '127.0.0.1';
+const mqttPort = process.env.MQTT_PORT || 1883;
+const TIMER_INTERVAL_BASE = parseInt(process.env.TIMER_INTERVAL) || 2000;
+
+// MQTT client setup with a client ID
+const mqttClient = mqtt.connect(`mqtt://${mqttHost}:${mqttPort}`, { clientId: 'mqtt_svc_423548fa' });
 mqttClient.on('connect', () => {
     console.log(`MQTT client connected to broker on port ${mqttPort}`);
+    // Publish the most recent stats to MQTT
+    publishStatsToMqtt();
 });
+
+async function publishStatsToMqtt() {
+    if (!statsData) {
+        console.log('No stats data available, skipping publish');
+    } else if (!mqttClient.connected) {
+        console.log('MQTT client not connected, skipping publish');
+    } else {
+        mqttClient.publish('stats/update', JSON.stringify(statsData));
+        console.log('MQTT message published');
+    }
+}
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -19,7 +36,6 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Configurable timer interval (in milliseconds)
-const TIMER_INTERVAL_BASE = parseInt(process.env.TIMER_INTERVAL) || 10000;
 const TIMER_INTERVAL_DRIFT = Math.floor(Math.random() * (750 - 250 + 1)) + 250; // artificial drift
 const TIMER_INTERVAL = TIMER_INTERVAL_BASE + (Math.random() < 0.5 ? -TIMER_INTERVAL_DRIFT : TIMER_INTERVAL_DRIFT);
 
@@ -66,14 +82,10 @@ async function updateStats() {
             console.log('Stats updated:', statsData);
 
             // Publish the updated stats to MQTT
-            mqttClient.publish('stats/update', JSON.stringify(statsData));
-            console.log('MQTT message published');
+            publishStatsToMqtt();
         }
     }, TIMER_INTERVAL);
 }
-
-// Start updating stats
-updateStats();
 
 // REST API endpoint
 app.get('/api/stats', (req, res) => {
@@ -84,3 +96,12 @@ app.get('/api/stats', (req, res) => {
 app.listen(port, () => {
     console.log(`HTTP Server is running on http://localhost:${port}`);
 });
+
+// Log initial stats
+console.log(`App running with stats:`, statsData);
+
+// Start updating stats
+updateStats();
+
+// Publish the updated stats to MQTT
+publishStatsToMqtt();
